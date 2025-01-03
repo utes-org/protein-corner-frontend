4f1cfecd6fa54cfd24f5622efb18550f95e31751
shazim8480 <troublemaker8480@gmail.com>
2025-01-03 19:43:29 +0600

    disabled google login

--- shop/src/components/auth/login-form.tsx
+++ shop/src/components/auth/login-form.tsx
@@ -1,35 +1,35 @@
-import { signIn } from 'next-auth/react';
-import Logo from '@/components/ui/logo';
-import Alert from '@/components/ui/alert';
-import Input from '@/components/ui/forms/input';
-import PasswordInput from '@/components/ui/forms/password-input';
-import Button from '@/components/ui/button';
-import { useTranslation } from 'next-i18next';
-import * as yup from 'yup';
-import { GoogleIcon } from '@/components/icons/google';
-import { useModalAction } from '@/components/ui/modal/modal.context';
-import { MobileIcon } from '@/components/icons/mobile-icon';
-import { Form } from '@/components/ui/forms/form';
-import { useLogin } from '@/framework/user';
-import type { LoginUserInput } from '@/types';
-import { AnonymousIcon } from '@/components/icons/anonymous-icon';
-import { useRouter } from 'next/router';
-import { Routes } from '@/config/routes';
-import { useSettings } from '@/framework/settings';
+import { signIn } from "next-auth/react";
+import Logo from "@/components/ui/logo";
+import Alert from "@/components/ui/alert";
+import Input from "@/components/ui/forms/input";
+import PasswordInput from "@/components/ui/forms/password-input";
+import Button from "@/components/ui/button";
+import { useTranslation } from "next-i18next";
+import * as yup from "yup";
+import { GoogleIcon } from "@/components/icons/google";
+import { useModalAction } from "@/components/ui/modal/modal.context";
+import { MobileIcon } from "@/components/icons/mobile-icon";
+import { Form } from "@/components/ui/forms/form";
+import { useLogin } from "@/framework/user";
+import type { LoginUserInput } from "@/types";
+import { AnonymousIcon } from "@/components/icons/anonymous-icon";
+import { useRouter } from "next/router";
+import { Routes } from "@/config/routes";
+import { useSettings } from "@/framework/settings";
 
 const loginFormSchema = yup.object().shape({
   email: yup
     .string()
-    .email('error-email-format')
-    .required('error-email-required'),
-  password: yup.string().required('error-password-required'),
+    .email("error-email-format")
+    .required("error-email-required"),
+  password: yup.string().required("error-password-required"),
 });
 function LoginForm() {
-  const { t } = useTranslation('common');
+  const { t } = useTranslation("common");
   const router = useRouter();
   const { openModal } = useModalAction();
   const { settings, isLoading: settingLoading } = useSettings();
-  const isCheckout = router.pathname.includes('checkout');
+  const isCheckout = router.pathname.includes("checkout");
   const { mutate: login, isLoading, serverError, setServerError } = useLogin();
 
   const guestCheckout = settings?.guestCheckout;
@@ -55,28 +55,28 @@ function LoginForm() {
         validationSchema={loginFormSchema}
         useFormProps={{
           defaultValues: {
-            email: 'customer@demo.com',
-            password: 'demodemo',
+            email: "customer@demo.com",
+            password: "demodemo",
           },
         }}
       >
         {({ register, formState: { errors } }) => (
           <>
             <Input
-              label={t('text-email')}
-              {...register('email')}
+              label={t("text-email")}
+              {...register("email")}
               type="email"
               variant="outline"
               className="mb-5"
               error={t(errors.email?.message!)}
             />
             <PasswordInput
-              label={t('text-password')}
-              {...register('password')}
+              label={t("text-password")}
+              {...register("password")}
               error={t(errors.password?.message!)}
               variant="outline"
               className="mb-5"
-              forgotPageRouteOnClick={() => openModal('FORGOT_VIEW')}
+              forgotPageRouteOnClick={() => openModal("FORGOT_VIEW")}
             />
             <div className="mt-8">
               <Button
@@ -84,7 +84,7 @@ function LoginForm() {
                 loading={isLoading}
                 disabled={isLoading}
               >
-                {t('text-login')}
+                {t("text-login")}
               </Button>
             </div>
           </>
@@ -94,11 +94,11 @@ function LoginForm() {
       <div className="relative flex flex-col items-center justify-center mt-8 mb-6 text-sm text-heading sm:mt-11 sm:mb-8">
         <hr className="w-full" />
         <span className="absolute -top-2.5 bg-light px-2 ltr:left-2/4 ltr:-ml-4 rtl:right-2/4 rtl:-mr-4">
-          {t('text-or')}
+          {t("text-or")}
         </span>
       </div>
       <div className="grid grid-cols-1 gap-4 mt-2">
-        <Button
+        {/* <Button
           className="!bg-social-google !text-light hover:!bg-social-google-hover"
           disabled={isLoading}
           onClick={() => {
@@ -107,15 +107,15 @@ function LoginForm() {
         >
           <GoogleIcon className="w-4 h-4 ltr:mr-3 rtl:ml-3" />
           {t('text-login-google')}
-        </Button>
+        </Button> */}
 
         <Button
           className="h-11 w-full !bg-gray-500 !text-light hover:!bg-gray-600 sm:h-12"
           disabled={isLoading}
-          onClick={() => openModal('OTP_LOGIN')}
+          onClick={() => openModal("OTP_LOGIN")}
         >
           <MobileIcon className="h-5 text-light ltr:mr-2 rtl:ml-2" />
-          {t('text-login-mobile')}
+          {t("text-login-mobile")}
         </Button>
 
         {isCheckout && guestCheckout && (
@@ -125,7 +125,7 @@ function LoginForm() {
             onClick={() => router.push(Routes.checkoutGuest)}
           >
             <AnonymousIcon className="h-6 text-light ltr:mr-2 rtl:ml-2" />
-            {t('text-guest-checkout')}
+            {t("text-guest-checkout")}
           </Button>
         )}
       </div>
@@ -133,12 +133,12 @@ function LoginForm() {
         <hr className="w-full" />
       </div>
       <div className="text-sm text-center text-body sm:text-base">
-        {t('text-no-account')}{' '}
+        {t("text-no-account")}{" "}
         <button
-          onClick={() => openModal('REGISTER')}
+          onClick={() => openModal("REGISTER")}
           className="font-semibold underline transition-colors duration-200 text-accent hover:text-accent-hover hover:no-underline focus:text-accent-hover focus:no-underline focus:outline-0 ltr:ml-1 rtl:mr-1"
         >
-          {t('text-register')}
+          {t("text-register")}
         </button>
       </div>
     </>
@@ -146,14 +146,14 @@ function LoginForm() {
 }
 
 export default function LoginView() {
-  const { t } = useTranslation('common');
+  const { t } = useTranslation("common");
   return (
     <div className="flex h-full min-h-screen w-screen flex-col justify-center bg-light py-6 px-5 sm:p-8 md:h-auto md:min-h-0 md:max-w-[480px] md:rounded-xl">
       <div className="flex justify-center">
         <Logo />
       </div>
       <p className="mt-4 mb-8 text-sm text-center text-body sm:mt-5 sm:mb-10 md:text-base">
-        {t('login-helper')}
+        {t("login-helper")}
       </p>
       <LoginForm />
     </div>
